f72df949391941c4a6b8527cb67249cf7af953d6
justinbushy <justinmbushphotography@gmail.com>
2017-10-08 09:43:08 -0600

    Figured out /api/user/:id test. All tests pass

--- test/user_test.js
+++ test/user_test.js
@@ -6,71 +6,126 @@ var chaiHTTP = require('chai-http');
 var app = require('../app.js');
 var should = chai.should();
 
+var mongoose = require('mongoose');
+var User = require('../models/userModel');
+
 chai.use(chaiHTTP);
 
 describe('Users', function() {
 
-    describe('/GET users', function() {
-        it('it should GET a list of all users', function(done) {
-            chai.request(app)
-                .get('/api/users')
-                .end(function(err, res) {
-                    res.should.have.status(200);
-                    res.body.should.have.property('data');
-                    res.body.data[0].should.have.property('_id');
-                    res.body.data[0].should.have.property('first_name');
-                    res.body.data[0].should.have.property('last_name');
-                    res.body.data[0].should.have.property('email');
-                    res.body.data[0].should.have.property('user_name');
-                    res.body.data[0].should.have.property('password');
-                    res.body.data.should.have.lengthOf(2);
-                    done();
-                });
-        });
-    });
-});
+    beforeEach(function(done) {
+        var user_1 = {
+            first_name: 'Justin',
+            last_name: 'Bush',
+            email: 'jbush@gmail.com',
+            user_name: 'jbush',
+            password: 'letmein'
+        };
 
-describe('Users', function() {
+        var user_2 = {
+            first_name: 'Bart',
+            last_name: 'Bush',
+            email: 'blackbart@gmail.com',
+            user_name: 'blackbart',
+            password: 'bones'
 
-    var user = { id: '59d5178545dd57374e25855d' }
-    describe('/GET users/:id', function() {
-        it('it should GET a list of 1 user', function(done) {
-            chai.request(app)
-                .get('/api/users/' + user.id)
-                .end(function (err, res) {
-                    res.should.have.status(200);
-                    res.body.should.have.property('data');
-                    res.body.data.should.have.property('_id').eql(user.id);
-                    res.body.data.should.have.property('first_name');
-                    res.body.data.should.have.property('last_name');
-                    res.body.data.should.have.property('email');
-                    res.body.data.should.have.property('user_name');
-                    res.body.data.should.have.property('password');
-                    done();
-                });
-        });
+        };
+
+        var new_user_1 = new User(user_1);
+        var new_user_2 = new User(user_2);
+
+        User.remove({}, function(err) {
+            if(err)
+                return err;
+
+        }).then(new_user_1.save(function(err){
+            if(err)
+                return err;
+        })).then(new_user_2.save(function(err){
+            if(err)
+                return err;
+            done();
+        }));
 
     });
-});
 
-describe('Users', function() {
 
-    var new_user = {
-        first_name: 'Chelsey',
-        last_name: 'Bush',
-        email: 'cbush@gmail.com',
-        user_name: 'cbush',
-        password: 'letmein'
-    }
-    describe('/POST users', function() {
-        it('it should POST and create new user', function(done) {
-            chai.request(app)
-                .post('/api/users')
-                .send(new_user)
-                .end(function(err, res) {
-                    res.should.have.status(200);
-                    done();
-                });
+    describe('Users', function () {
+
+        describe('/GET users', function () {
+            it('it should GET a list of all users', function (done) {
+                chai.request(app)
+                    .get('/api/users')
+                    .end(function (err, res) {
+                        res.should.have.status(200);
+                        res.body.should.have.property('data');
+                        res.body.data[0].should.have.property('_id');
+                        res.body.data[0].should.have.property('first_name');
+                        res.body.data[0].should.have.property('last_name');
+                        res.body.data[0].should.have.property('email');
+                        res.body.data[0].should.have.property('user_name');
+                        res.body.data[0].should.have.property('password');
+                        res.body.data.should.have.lengthOf(2);
+                        done();
+                    });
+            });
         });
     });
+
+    describe('GET /api/users/:user_id', function() {
+        it('it should GET 1 user by the given id', function(done) {
+            var new_user = {
+                first_name: 'Nala',
+                last_name: 'Bush',
+                email: 'furrynala@gmail.com',
+                user_name: 'babynala',
+                password: 'scratch'
+            };
+
+            var insert_user = new User(new_user);
+
+            insert_user.save(function(err, user) {
+                chai.request(app)
+                    .get('/api/users/' + user._id)
+                    .send(insert_user)
+                    .end(function(err, res) {
+                        res.should.have.status(200);
+                        res.body.should.have.property('data');
+                        res.body.should.have.property('status');
+                        res.body.should.have.property('message');
+                        res.body.data.should.have.property('_id').eql('' + insert_user._id);
+                        res.body.data.should.have.property('first_name').eql('' + insert_user.first_name);
+                        res.body.data.should.have.property('last_name').eql('' + insert_user.last_name);
+                        res.body.data.should.have.property('email').eql('' + insert_user.email);
+                        res.body.data.should.have.property('user_name').eql('' + insert_user.user_name);
+                        res.body.data.should.have.property('password').eql('' + insert_user.password);
+                        done();
+                    });
+            });
+
+        });
+    });
+
+    describe('Users', function () {
+
+        var new_user = {
+            first_name: 'Chelsey',
+            last_name: 'Bush',
+            email: 'cbush@gmail.com',
+            user_name: 'cbush',
+            password: 'letmein'
+        }
+        describe('/POST users', function () {
+            it('it should POST and create new user', function (done) {
+                chai.request(app)
+                    .post('/api/users')
+                    .send(new_user)
+                    .end(function (err, res) {
+                        res.should.have.status(200);
+                        done();
+                    });
+            });
+        });
+    });
+
 });

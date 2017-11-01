06ef456034a57c980523ba9dd9b881d0b921d079
justinbushy <justinmbushphotography@gmail.com>
2017-11-01 13:19:58 -0600

    Added middleware to allow CORS

--- app.js
+++ app.js
@@ -54,6 +54,12 @@ app.use(function (req, res, next) {
   }
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Routes
 app.use('/', index);
 app.use('/api', userRoutes);
